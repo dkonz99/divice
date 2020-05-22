cb9da6f9d580dc3d6ed9f051fee85dab2a2cc369
Дмитрий <52136128+oneexperience11@users.noreply.github.com>
2020-05-22 15:19:26 +0300

    Исправлена резина и пройдена проверка на валидность

--- source/scripts/main-slide.js
+++ source/scripts/main-slide.js
@@ -2,12 +2,10 @@ export function mainSlide() {
   let mainSlideLink = document.querySelectorAll(".main-slide__radio"); 
   let mainSlidePopup = document.querySelectorAll(".main-slide");
   let maimSlide1 = document.querySelector(".main-slide--1");
-  
 
   if (mainSlidePopup[0]) {
       mainSlidePopup[0].classList.add("modal-show");
       for (let i = 0; i<mainSlidePopup.length; i++) {
-         
           mainSlideLink[i].addEventListener("click", function(evt) {             
                 mainSlidePopup[0].classList.remove("modal-show");
                 mainSlidePopup[mainSlidePopup.length-1].classList.remove("modal-show");
@@ -17,9 +15,7 @@ export function mainSlide() {
                 }
                 if (mainSlidePopup[i+1]) {
                   mainSlidePopup[i+1].classList.remove("modal-show");
-                }
-                
-            
+                } 
           });
       }
   }
